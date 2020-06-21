6c3b14b9424a9d4ad504101d338badd90109cfb3
Anton <anton.pohli@gmail.com>
2020-06-21 16:31:09 +0200

    Added retry logic to rethinkdb connecting as rethinkdb might not immidieatly be available

--- server/database.js
+++ server/database.js
@@ -1,7 +1,43 @@
 const r = require('rethinkdb');
 
+
+let firstTry = null
+let retried = false;
 function connectDatabase(callback){
-    r.connect({"host": process.env.RETHINK_DB_HOST || "localhost", "port":process.env.RETHINK_DB_PORT || 28015, "db":process.env.RETHINK_DB_DATABASE || "linksOfTheAnvil"}, (err, conn) => onStartup(err, conn, callback));
+    if(firstTry === null)firstTry = new Date();
+    try{
+        r.connect({"host": process.env.RETHINK_DB_HOST || "localhost", "port":process.env.RETHINK_DB_PORT || 28015, "db":process.env.RETHINK_DB_DATABASE || "linksOfTheAnvil"}, 
+        (err, conn) => 
+        {
+            if(err){
+                console.error("Could not open a connection to the database");
+                if(new Date() - firstTry < 1000 * 30){
+                    console.error("Retrying");
+                    setTimeout(() => connectDatabase(callback), 200);
+                }else{
+                    console.error("Retried for over 30s, the database is down. Exiting");
+                    console.error(err.message);
+                    process.exit(1);
+                }
+            }else{               
+                try{
+                    onStartup(err, conn, callback);
+                }catch(e){
+                    if(new Date() - firstTry < 1000 * 30){
+                        console.error("Retrying");
+                        setTimeout(() => connectDatabase(callback), 1000);
+                    }else{
+                        console.error("Retried for over 30s, the database is down. Exiting");
+                        console.error(err.message);
+                        process.exit(1);
+                    }
+                }
+            }
+        }            
+        );
+    }catch{
+
+    }   
 }
 
 function onStartup(err, conn, callback){
@@ -15,6 +51,17 @@ function onStartup(err, conn, callback){
         console.log("Database is initialized, starting web server");
         callback();
     }).error(err => {
+        if(!err.message.includes("does not exist")){
+            if(new Date() - firstTry < 1000 * 30){
+                console.error("Retrying");
+                setTimeout(() => connectDatabase(callback), 1000);
+            }else{
+                console.error("Retried for over 30s, the database is down. Exiting");
+                console.error(err.message);
+                process.exit(1);
+            }
+            return;
+        }
         console.log("Initializing Database");
         r.dbCreate("linksOfTheAnvil").run(conn).finally(() => {
             return r.tableCreate("worlds").run(conn);
